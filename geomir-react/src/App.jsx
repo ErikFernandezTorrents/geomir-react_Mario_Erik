aa4f2b9dcaaa8746edcbdb91f1942c7b72ea9409
Erik Fernandez Torrents <erk.rueda12@gmail.com>
2023-03-08 19:24:01 +0100

    acabando 3.4.0

--- geomir-react/src/App.jsx
+++ geomir-react/src/App.jsx
@@ -1,9 +1,9 @@
 import './App.css'
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { LoginRegister } from './auth/LoginRegister';
 import { UserContext } from "./userContext";
 import { Header } from './layout/Header';
-import {About} from './About'
+import { About } from './About'
 import { Routes, Route } from "react-router-dom";
 import { NotFound } from './NotFound';
 import { Place } from './places/Place';
@@ -23,47 +23,77 @@ import { PlacesMenu } from './places/PlacesMenu';
 import { ToDos } from './todos/ToDos';
 import './styles.css'
 import PlaceMarks from './places/PlaceMarks';
+import { db } from "./firebase";
+import {
 
+  collection,
+
+  getDocs,
+
+} from "firebase/firestore";
 
 const App = () => {
   let [authToken, setAuthToken] = useState("");
   let [usuari, setUsuari] = useState("");
+  const todosCollectionRef = collection(db, "todos")
+  const getTodos = async () => {
+
+    const dades = await getDocs(todosCollectionRef);
+
+    const desar = [];
+
+    dades.docs.map((v) => {
+
+      desar.push(v.data());
+
+    });
+    console.log(desar)
+
+    localStorage.setItem("todos",JSON.stringify(desar))
+
+  };
+
+  useEffect(() => {
+
+    getTodos();
+    
+    }, [authToken]);
   return (
     <>
-      <UserContext.Provider value={{  usuari, setUsuari,authToken, setAuthToken }}  >
+      <UserContext.Provider value={{ usuari, setUsuari, authToken, setAuthToken }}  >
         {authToken ? (
-            <>
-              <Header/>
-              <div className='divPrincipal'>
-                <Routes>
-                  <Route path='/about'element={<About/>}/>
-                  <Route path='/notfound'element={<NotFound/>}/>
-                  <Route path="/posts/list" element={ <><PostsMenu/><PostsList/> </> } />
-                  <Route path="/posts/add" element={ <><PostsMenu/><PostAdd/></> } /> 
-                  <Route path="/posts/edit/:id" element={ <><PostsMenu/><PostEdit/></> } /> 
-                  <Route path="/posts/grid" element={ <><PostsMenu/><PostsGrid/></> } /> 
-                  <Route path="/posts/:id" element={ <><PostsMenu/><Post/></> } />
-                  <Route path='/posts'element={<Post/>}/>
-                
-                  <Route path='/about'element={<About/>}/>
-                  <Route path='/notfound'element={<NotFound/>}/>
-                  <Route path="/places/list" element={ <><PlacesMenu/><PlacesList/> </> } />
-                  <Route path="/places/add" element={ <><PlacesMenu/><PlaceAdd/></> } /> 
-                  <Route path="/places/edit/:id" element={ <><PlacesMenu/><PlaceEdit/></> } /> 
-                  <Route path="/places/grid" element={ <><PlacesMenu/><PlacesGrid/></> } /> 
-                  <Route path="/places/:id" element={ <><PlacesMenu/><Place/></> } />
-                  <Route path='/todos'element={<ToDos/>}/>
-                  <Route path='/places/marks'element={<PlaceMarks/>}/>
-                </Routes>
-              </div>
-            </>
-          ) : (
-              <LoginRegister />
-          )}
+          <>
+            <Header />
+            <div className='divPrincipal'>
+              <Routes>
+                <Route path='/about' element={<About />} />
+                <Route path='/notfound' element={<NotFound />} />
+                <Route path="/posts/list" element={<><PostsMenu /><PostsList /> </>} />
+                <Route path="/posts/add" element={<><PostsMenu /><PostAdd /></>} />
+                <Route path="/posts/edit/:id" element={<><PostsMenu /><PostEdit /></>} />
+                <Route path="/posts/grid" element={<><PostsMenu /><PostsGrid /></>} />
+                <Route path="/posts/:id" element={<><PostsMenu /><Post /></>} />
+                <Route path='/posts' element={<Post />} />
+
+                <Route path='/about' element={<About />} />
+                <Route path='/notfound' element={<NotFound />} />
+                <Route path="/places/list" element={<><PlacesMenu /><PlacesList /> </>} />
+                <Route path="/places/add" element={<><PlacesMenu /><PlaceAdd /></>} />
+                <Route path="/places/edit/:id" element={<><PlacesMenu /><PlaceEdit /></>} />
+                <Route path="/places/grid" element={<><PlacesMenu /><PlacesGrid /></>} />
+                <Route path="/places/:id" element={<><PlacesMenu /><Place /></>} />
+                <Route path='/todos' element={<ToDos />} />
+                <Route path='/places/marks' element={<PlaceMarks />} />
+              </Routes>
+            </div>
+          </>
+        ) : (
+          <LoginRegister />
+        )}
       </UserContext.Provider>
     </>
   );
-  
+
 }
 
 export default App
--- geomir-react/src/todos/ToDos.jsx
+++ geomir-react/src/todos/ToDos.jsx
@@ -2,35 +2,83 @@ import React, { useEffect } from 'react'
 import { useSelector } from 'react-redux';
 import { ToDo } from "./ToDo";
 import { ToDoAdd } from "./ToDoAdd";
+import { db } from "../firebase";
+import {
+
+  doc,
+
+  getDocs,
+
+  deleteDoc,
+
+  addDoc,
+
+} from "firebase/firestore";
 
 export const ToDos = () => {
-    //const [todos, dispatchTodos] = useReducer(todoReducer, initialState,init);
-    
-    const { todos } = useSelector(state => state.todos)
+  //const [todos, dispatchTodos] = useReducer(todoReducer, initialState,init);
 
-    useEffect ( ()=>{
-      localStorage.setItem('todos',JSON.stringify(todos))
-    },[todos])
+  const { todos } = useSelector(state => state.todos)
+
+  const todosCollectionRef = collection(db, "todos")
+
+  const synchronize = async () => {
+
+    // Obtenim tots els todos per adesprés esobrrar-los
+
+    const dades = await getDocs(todosCollectionRef);
+
+    // Esborrem tots els todos
+
+    // aquest sistema no es recomana en entorn web,
+
+    // però no hi ha un altra opció
+
+    dades.docs.map((v) => {
+
+      deleteDoc(doc(db, "todos", v.id));
+
+    });
+
+    // Afegim tots els todos de nou
+
+    todos.map((p) => {
+
+      addDoc(todosCollectionRef, {
+        id: p.id,
+
+        description: p.description,
+
+        done: p.done,
+      });
+
+    });
+
+  };
+
+  useEffect(() => {
+    localStorage.setItem('todos', JSON.stringify(todos))
+  }, [todos])
 
   return (
-      <>
-        <h1>Llistat de Tascas</h1>
-        <ToDoAdd/>
-        <table id='tablePlaceList'>
-          <tbody>
-            <tr id='tr1PlaceList'>
-              <th>Id</th>
-              <th>Descripción</th>
-            </tr>
-            <div>  
-                {todos.map((todo) => (
-                <tr><ToDo key={todo.id} todo={todo}/></tr>
-              
-                ))}
-            </div>
-          </tbody>
-        </table>
-      </>
+    <>
+      <h1>Llistat de Tascas</h1>
+      <ToDoAdd />
+      <table id='tablePlaceList'>
+        <tbody>
+          <tr id='tr1PlaceList'>
+            <th>Id</th>
+            <th>Descripción</th>
+          </tr>
+          <div>
+            {todos.map((todo) => (
+              <tr><ToDo key={todo.id} todo={todo} /></tr>
+
+            ))}
+          </div>
+        </tbody>
+      </table>
+    </>
   )
 }
 export default ToDos
